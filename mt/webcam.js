069a1c7a279e4fbfd200c380a5b3c28e6321d10b
jjwwcc <jjwwcc@gmail.com>
2023-11-07 21:50:04 -0800

    Update webcam.js

--- mt/webcam.js
+++ mt/webcam.js
@@ -16,8 +16,21 @@ if (!token) {
 let isCapturing = false;
 // Initialize the webcam and set event listeners
 function initializeWebcam() {
+    if (isCapturing) {
+        document.getElementById('start-camera').innerHTML = 'Start Camera';
+        document.getElementById('webcam').style.display = 'none';
+        document.getElementById('drop_area').style.display = 'block';
+        const video = document.getElementById('webcam');
+        video.srcObject.getTracks().forEach(track => track.stop());
+        isCapturing = !isCapturing;
+        return;
+    }
+    isCapturing = !isCapturing;
+    document.getElementById('start-camera').innerHTML = 'Stop Camera';
     const video = document.getElementById('webcam');
-    navigator.mediaDevices.getUserMedia({ video:  { facingMode: "environment" } })
+    video.style.display = 'block';
+    document.getElementById('drop_area').style.display = 'none';
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
         .then(stream => {
             video.srcObject = stream;
         })
@@ -29,7 +42,6 @@ function initializeWebcam() {
 
 // Function to capture image from webcam and process it
 function captureImage() {
-    isCapturing = !isCapturing;
     const video = document.getElementById('webcam');
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
@@ -73,12 +85,18 @@ function imageAsk() {
     document.getElementById('message-container').style.display = 'none';
     document.getElementById('chatbox').style.display = 'block';
     document.getElementById('chatbox-content').innerHTML = '';
+    document.getElementById('webcam').style.display = 'none';
+    // change full screen background image
+    document.body.style.backgroundImage = `url(data:image/jpeg;base64,${base64Image})`;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.getElementById('msg').value = '';
     processImage();
 }
 
 function processImage() {
     toggleLoader(true);
-    console.log(base64Image);
 
     fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
@@ -127,7 +145,16 @@ function appendToChatbox(message, isUserMessage = false) {
 
     chatboxContent.innerHTML = `<div class="message-content">${message}</div>
                         <div class="timestamp">${timestamp}</div>`;
-
+    // write assistant message to prompt
+    prompt.messages.push({
+        role: "assistant",
+        content: [
+            {
+                type: "text",
+                text: message
+            }
+        ]
+    });
     // messageElement.innerHTML = `<div class="message-content">${message}</div>
     //                             <div class="timestamp">${timestamp}</div>`;
     // if (chatbox.firstChild) {
@@ -135,6 +162,22 @@ function appendToChatbox(message, isUserMessage = false) {
     // } else {
     //     chatbox.appendChild(messageElement);
     // }
+}
+
+// chat again
+function chatAgain() {
+    var userInput = document.getElementById('userInputText').value;
+    prompt.messages.push({
+        role: "user",
+        content: [
+            {
+                type: "text",
+                text: userInput
+            }
+        ]
+    });
+    document.getElementById('userInputText').value = '';
+    processImage();
 }
 
 // Function to switch the camera source
@@ -152,6 +195,7 @@ function switchCamera() {
         // Stop any previous stream
         if (video.srcObject) {
             video.srcObject.getTracks().forEach(track => track.stop());
+            document.getElementById('start-camera').innerHTML = 'Start Camera';
         }
 
         // Start a new stream with the new constraints
@@ -165,10 +209,67 @@ function switchCamera() {
     };
 }
 
+// drog and drop event
+function dragOverHandler() {
+    var dropArea = document.getElementById('drop_area');
+
+    // 阻止默认行为
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    // 高亮显示拖拽区域
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
+    });
+
+    function highlight(e) {
+        dropArea.classList.add('highlight');
+    }
+
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight');
+    }
+
+    // 处理拖拽的文件
+    dropArea.addEventListener('drop', handleDrop, false);
+
+    function handleDrop(e) {
+        var dt = e.dataTransfer;
+        var files = dt.files;
+        // if file is image, convert to base64
+        if (files[0].type.indexOf('image') > -1) {
+            var reader = new FileReader();
+            reader.readAsDataURL(files[0]);
+            reader.onload = function () {
+                base64Image = reader.result.split(',')[1];
+                document.getElementById('image').src = `data:image/jpeg;base64,${base64Image}`;
+                document.getElementById('image').style.display = 'block';
+                document.getElementById('message-container').style.display = 'flex';
+                document.getElementById('chatbox').style.display = 'none';
+            }
+        }
+    }
+}
+
+// onclick uploadButton
+function uploadImage() {
+    document.getElementById('file').click();
+}
+
 // Event listeners
 document.addEventListener('DOMContentLoaded', function () {
-    initializeWebcam();
-
+    // initializeWebcam();
+    dragOverHandler();
     document.getElementById('capture').addEventListener('click', captureImage);
     document.getElementById('switch-camera').addEventListener('click', switchCamera());
     document.getElementById('imageAsk').addEventListener('click', imageAsk);
@@ -180,4 +281,19 @@ document.addEventListener('DOMContentLoaded', function () {
             imageAsk();
         }
     });
+    document.getElementById('uploadButton').addEventListener('click', uploadImage);
+    // upload file
+    document.getElementById('file').addEventListener('change', function (e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            base64Image = reader.result.split(',')[1];
+            document.getElementById('image').src = `data:image/jpeg;base64,${base64Image}`;
+            document.getElementById('image').style.display = 'block';
+            document.getElementById('message-container').style.display = 'flex';
+            document.getElementById('chatbox').style.display = 'none';
+        }
+    });
+    document.getElementById('userInputButton').addEventListener('click', chatAgain);
 });
